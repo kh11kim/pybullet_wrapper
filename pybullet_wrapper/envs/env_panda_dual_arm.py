45dd6b9740cd30a35ed2f6fb53573f87d71f5308
Kanghyun Kim <kanghyun777@naver.com>
2022-02-26 01:23:06 +0900

    add pandadualarmgym

--- pybullet_wrapper/envs/env_panda_dual_arm.py
+++ pybullet_wrapper/envs/env_panda_dual_arm.py
@@ -4,6 +4,10 @@ from ..core import Bullet
 from ..scene_maker import BulletSceneMaker
 from ..collision_checker import BulletCollisionChecker
 from ..robots import PandaDualArm
+import gym
+import pybullet as p
+from gym import spaces
+from gym.envs.registration import register
 
 class PandaDualArmEnvBase:
     def __init__(self, render=False, arm_distance=0.4):
@@ -62,6 +66,7 @@ class PandaDualArmEnvBase:
                     self.robot.get_random_joint_angles(set=True)
                     if not self.checker.is_collision():
                         random_joint_angles = self.robot.get_joint_angles()
+                        break
         return random_joint_angles
     
     def get_random_free_configuration_in_taskspace(self, panda1_first=True):
@@ -101,3 +106,158 @@ class PandaDualArmEnvBase:
             if self.checker.is_collision():
                 result = True
         return result
+
+class PandaDualArmGymEnv(PandaDualArmEnvBase, gym.GoalEnv):
+    """ joint
+    """
+    def __init__(self, render=False, reward_type="joint", level=0.1):
+        self.reward_type = reward_type
+        self.level = level
+        super().__init__(render=render,  arm_distance=0.4)
+        self.n_obs = 1
+        self.task_ll = np.array([-1.2, -1.2, -1.2])
+        self.task_ul = np.array([1.2, 1.2, 1.2])
+        self.observation_space = spaces.Dict(dict(
+            observation=spaces.Box(-1, 1, shape=(1,), dtype=np.float32),    
+            achieved_goal=spaces.Box(
+                self.robot.joint_ll, 
+                self.robot.joint_ul, 
+                shape=(self.robot.n_joints,), 
+                dtype=np.float32
+            ),
+            desired_goal=spaces.Box(
+                self.robot.joint_ll, 
+                self.robot.joint_ul, 
+                shape=(self.robot.n_joints,), 
+                dtype=np.float32
+            ),
+        ))
+        self.action_space = spaces.Box(-1.0, 1.0, shape=(self.robot.n_joints,), dtype=np.float32)
+        self._goal = None
+        self.eps = 0.1
+        self.max_joint_change = 0.1
+    
+    def set_debug_mode(self):
+        self.bullet.physics_client.configureDebugVisualizer(p.COV_ENABLE_GUI, 1)
+        self.bullet.physics_client.configureDebugVisualizer(p.COV_ENABLE_MOUSE_PICKING, 1)
+        joint_angles = self.robot.get_joint_angles()
+        self.param_idxs = []
+        for idx in self.robot.ctrl_joint_idxs:
+            name = self.robot.joint_info[idx]["joint_name"].decode("utf-8")
+            param_idx = self.bullet.physics_client.addUserDebugParameter(
+                name,-4,4,
+                joint_angles[idx]
+            )
+            self.param_idxs.append(param_idx)
+    
+    def do_debug_mode(self):
+        joint_param_values = []
+        for param in self.param_idxs:
+            joint_param_values.append(p.readUserDebugParameter(param))
+        self.robot.set_joint_angles(joint_param_values)
+
+    @property
+    def goal(self):
+        return self._goal.copy()
+    
+    @goal.setter
+    def goal(self, arr: np.ndarray):
+        self._goal = arr
+    
+    def get_observation(self):
+        return dict(
+            observation=0.,
+            achieved_goal=self.robot.get_joint_angles(),
+            desired_goal=self.goal,
+        )
+    
+    def is_limit(self):
+        joint_angles = self.robot.get_joint_angles()
+        is_ll = np.any(joint_angles == self.robot.joint_ll)
+        is_ul = np.any(joint_angles == self.robot.joint_ul)
+        return is_ll | is_ul
+
+    def set_action(self, action: np.ndarray):
+        joint_prev = self.robot.get_joint_angles()
+        action = action.copy()
+        action = np.clip(action, self.action_space.low, self.action_space.high)
+        joint_target = self.robot.get_joint_angles()
+        joint_target += action * self.max_joint_change
+        joint_target = np.clip(joint_target, self.robot.joint_ll, self.robot.joint_ul)
+        if self.checker.is_collision() & self.is_limit():
+            self.robot.set_joint_angles(joint_prev)
+            self._collision_flag = True
+        self._collision_flag = False
+        self.robot.set_joint_angles(joint_target)
+
+    # def distance(self, pose1: np.ndarray, pose2: np.ndarray) -> np.float64:
+    #     pos1, orn_mat1 = pose1[:3], pose1[3:].reshape(3,3)
+    #     pos2, orn_mat2 = pose2[:3], pose2[3:].reshape(3,3)
+    #     diff_mat = orn_mat1.T@orn_mat2
+    #     delta = np.linalg.norm(pos1 - pos2)
+    #     delta_theta = np.arccos((np.trace(diff_mat)-1)/2)
+    #     return delta + delta_theta/(np.pi * 2)
+
+    def is_success(self, joint_curr: np.ndarray, joint_goal: np.ndarray):
+        return np.linalg.norm(joint_curr - joint_goal) < self.eps
+
+    def reset(self):
+        random_joint1 = self.get_random_configuration(collision_free=True)
+        while True:
+            random_joint2 = self.get_random_configuration(collision_free=False)
+            goal = random_joint1 + (random_joint2 - random_joint1) * self.level
+            if not self.is_collision(goal):
+                self.robot.set_joint_angles(goal)
+                goal_ee = self.robot.get_ee_position()
+                break
+        self.start = random_joint1
+        self.goal = goal
+        self.robot.set_joint_angles(self.start)
+        start_ee = self.robot.get_ee_position()
+
+        if self.is_render:
+            ee1, ee2 = start_ee[:3], start_ee[3:]
+            self.scene_maker.view_position("goal1", goal_ee[:3])
+            self.scene_maker.view_position("goal2", goal_ee[3:])
+            self.scene_maker.view_position("curr1", start_ee[:3])
+            self.scene_maker.view_position("curr2", start_ee[3:])
+        return self.get_observation()
+
+
+    def step(self, action: np.ndarray):
+        self.set_action(action)
+        obs_ = self.get_observation()
+        done = False
+        info = dict(
+            is_success=self.is_success(obs_["achieved_goal"], obs_["desired_goal"]),
+            actions=action.copy(),
+            collisions=self._collision_flag,
+        )
+        reward = self.compute_reward(obs_["achieved_goal"], obs_["desired_goal"], info)
+        if self.is_render:
+            self.scene_maker.view_position("curr1", self.robot.get_ee_position()[:3])
+            self.scene_maker.view_position("curr2", self.robot.get_ee_position()[3:])
+        return obs_, reward, done, info
+
+    def compute_reward(self, achieved_goal, desired_goal, info):
+        if len(achieved_goal.shape) == 2:
+            actions = np.array([i["actions"] for i in info])
+            collisions = np.array([i["collisions"] for i in info])
+        else:
+            actions = info["actions"]
+            collisions = info["collisions"]
+        r = - np.linalg.norm(achieved_goal-desired_goal, axis=-1)
+
+        if "action" in self.reward_type:
+            r -= np.linalg.norm(actions, axis=-1) / 10
+        
+        if "col" in self.reward_type:
+            r -= collisions * 1.
+        
+        return r
+
+register(
+    id='MyPandaDualArmReach-v0',
+    entry_point='dualarm.pybullet_wrapper:PandaDualArmGymEnv',
+    max_episode_steps=100,
+)
--- test.py
+++ test.py
@@ -1,12 +1,13 @@
-from pybullet_wrapper import Bullet, BulletCollisionChecker, BulletRobot, BulletSceneMaker, PandaDualArmEnv
+from pybullet_wrapper import PandaDualArmGymEnv
 import numpy as np
 import pybullet as p
 
-env = PandaDualArmEnv(render=True)
-pos = env.robot.panda1.get_ee_position()
-orn = env.robot.panda1.get_ee_orientation()
-joint_angles = env.robot.panda1.inverse_kinematics(pos, orn)
-env.scene_maker.view_frame("ee1", pos, orn)
-env.bullet.render()
+env = PandaDualArmGymEnv(render=True)
+obs = env.reset()
+for i in range(100):
+    obs = env.reset()
+    env.render("human")
+    #action = env.action_space.sample()
+    #obs_, reward, done, info = env.step(action)
 
 input()
