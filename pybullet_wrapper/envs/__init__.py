9e6686b18bd26314fe2a18c17bc995672618e838
Kanghyun Kim <kanghyun777@naver.com>
2022-03-01 01:53:56 +0900

    make env_panda_new(refactoring)

--- pybullet_wrapper/envs/__init__.py
+++ pybullet_wrapper/envs/__init__.py
@@ -1,4 +1,5 @@
 from .env_panda import *
+from .env_panda_new import *
 from .env_panda_dual_arm import *
 from .env_panda_col_avoid import *
 from .env_panda_dual_arm_col_avoid import *
--- test.py
+++ test.py
@@ -4,12 +4,18 @@ import numpy as np
 import pybullet as p
 import time
 
-env = PandaDualArmGymEnvColAvoid(render=True, reward_type="joint_col_obs")
+#env = PandaGymNewEnv(render=True, reward_type="")
 #env = PandaDualArmGymEnv(render=True, level=0.1)
 #env = gym.make("MyPandaDualArmReach-v0")
-env.set_debug_mode()
+#env.set_debug_mode()
+rewards = []
 obs = env.reset()
-
+for i in range(100):
+    obs = env.reset()
+    action = env.action_space.sample()
+    obs_, reward, done, info = env.step(action)
+    rewards.append(reward)
+print(f"reward:{np.mean(rewards)}")
 while True:
     #obs = env.reset()
     #env.render("human")
